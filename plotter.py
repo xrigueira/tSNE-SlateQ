ed17bec241ba89ce52f8517744783659c920af42
Xurxo Rigueira <xrigueiradiaz@gmail.com>
2025-03-20 13:32:07 -0700

    Add new results

--- plotter.py
+++ plotter.py
@@ -1,6 +1,8 @@
 import numpy as np
 from matplotlib import pyplot as plt
 plt.style.use('ggplot')
+from matplotlib import rcParams
+rcParams['font.family'] = 'monospace'
 
 """This file is just for faster plotting in case of trying different layouts"""
 
@@ -11,27 +13,76 @@ y = np.load('y.npy')
 fig, ax = plt.subplots(subplot_kw={"projection": "3d"}, figsize=(11.7,8.27))
 scatter = ax.scatter(X_embedded[:,0], X_embedded[:,1], X_embedded[:,2], c=y, cmap='brg', marker='o', edgecolors='w')
 
-ax.set(xlim=(-30, 30), ylim=(-20, 20), zlim=(-25, 25), 
-        title='t-distributed Stochastic Neighbor Embedding (t-SNE)',
-        xlabel='Component 1', ylabel='Component 2', zlabel='Component 3')
+ax.set(xlim=(-30, 30), ylim=(-20, 20), zlim=(-25, 25))
 
-legend = ax.legend(*scatter.legend_elements(), loc='best', title='Quality index')
+ax.set_title('(a)', fontname='Arial', fontsize=16)
+ax.set_xlabel('Component 1', fontname='Arial', fontsize=14)
+ax.set_ylabel('Component 2', fontname='Arial', fontsize=14)
+ax.set_zlabel('Component 3', fontname='Arial', fontsize=14)
+
+legend = ax.legend(*scatter.legend_elements(), loc='best', title='Quality index', fontsize=14, title_fontsize='13', prop={'family':'Arial'})
+plt.setp(legend.get_title(), fontname='Arial')
 
 ax.add_artist(legend)
 
 plt.show()
 
-# # Plot a 2D projection
-# fig, ax = plt.subplots(figsize=(11.7,8.27))
+# Plot first 2D projection
+fig, ax = plt.subplots(figsize=(11.7,8.27))
 
-# scatter = ax.scatter(X_embedded[:,0], X_embedded[:,1], c=y, cmap='brg', marker='o', edgecolors='w') # Plotting a 2D projection
+scatter = ax.scatter(X_embedded[:,0], X_embedded[:,1], c=y, cmap='brg', marker='o', edgecolors='w') # Plotting a 2D projection
 
-# ax.set(xlim=(-30, 30), ylim=(-20, 20), 
-#         title='t-distributed Stochastic Neighbor Embedding (t-SNE)',
-#         xlabel='Component 1', ylabel='Component 2')
+ax.set(xlim=(-30, 30), ylim=(-20, 20))
 
-# legend = ax.legend(*scatter.legend_elements(), loc='best', title='Quality index')
+ax.set_title('(a)', fontname='Arial', fontsize=16)
+ax.set_xlabel('Component 1', fontname='Arial', fontsize=14)
+ax.set_ylabel('Component 2', fontname='Arial', fontsize=14)
 
-# ax.add_artist(legend)
+legend = ax.legend(*scatter.legend_elements(), loc='best', title='Quality index', fontsize=14, title_fontsize='13', prop={'family':'Arial'})
+plt.setp(legend.get_title(), fontname='Arial')
+
+ax.add_artist(legend)
 
 # plt.show()
+
+plt.savefig('Figures/figure1a.pdf', format='pdf', dpi=300, bbox_inches='tight')
+
+# Plot second 2D projection
+fig, ax = plt.subplots(figsize=(11.7,8.27))
+
+scatter = ax.scatter(X_embedded[:,0], X_embedded[:,2], c=y, cmap='brg', marker='o', edgecolors='w') # Plotting a 2D projection
+
+ax.set(xlim=(-30, 30), ylim=(-25, 25))
+
+ax.set_title('(b)', fontname='Arial', fontsize=16)
+ax.set_xlabel('Component 1', fontname='Arial', fontsize=14)
+ax.set_ylabel('Component 3', fontname='Arial', fontsize=14)
+
+legend = ax.legend(*scatter.legend_elements(), loc='best', title='Quality index', fontsize=14, title_fontsize='13', prop={'family':'Arial'})
+plt.setp(legend.get_title(), fontname='Arial')
+
+ax.add_artist(legend)
+
+# plt.show()
+
+plt.savefig('Figures/figure1b.pdf', format='pdf', dpi=300, bbox_inches='tight')
+
+# Plot third 2D projection
+fig, ax = plt.subplots(figsize=(11.7,8.27))
+
+scatter = ax.scatter(X_embedded[:,1], X_embedded[:,2], c=y, cmap='brg', marker='o', edgecolors='w') # Plotting a 2D projection
+
+ax.set(xlim=(-20, 20), ylim=(-25, 25))
+
+ax.set_title('(c)', fontname='Arial', fontsize=16)
+ax.set_xlabel('Component 2', fontname='Arial', fontsize=14)
+ax.set_ylabel('Component 3', fontname='Arial', fontsize=14)
+
+legend = ax.legend(*scatter.legend_elements(), loc='best', title='Quality index', fontsize=14, title_fontsize='13', prop={'family':'Arial'})
+plt.setp(legend.get_title(), fontname='Arial')
+
+ax.add_artist(legend)
+
+# plt.show()
+
+plt.savefig('Figures/figure1c.pdf', format='pdf', dpi=300, bbox_inches='tight')
